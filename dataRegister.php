391d82ac9c3de880ff21922546e21ded5de1f12e
NicolasGlassey <nicolas.glassey@heig-vd.ch>
2019-01-06 15:58:05 +0100

    Step02 - End

---
+++ dataRegister.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Author   : nicolas.glassey@cpnv.ch
+ * Project  : 133
+ * Created  : 31.12.2018 - 10:35
+ *
+ * Last update :    [01.12.2018 author]
+ *                  [add $logName in function setFullPath]
+ * Git source  :    [link]
+ */
+
+//region Global variables
+$dataForm = $_POST;
+//endregion Global variables
+
+
+
+
+
+
